5bdcec923091a528504517f67e23e0e1c8c3cd3f
Oz Blumenfeld <ozb077@gmail.com>
2026-03-12 16:31:03 -0400

    Improving rag pipeline, more chunks with less overlap and avoid passing the context twice

---
+++ common/workflows/workflow.py
@@ -0,0 +1,16 @@
+
+from abc import ABC, abstractmethod
+
+
+class Workflow(ABC):
+
+
+    def __init__(self) -> None:
+        pass
+        # next_run_time:
+    @abstractmethod
+    def run_task():
+        pass
+
+
+
